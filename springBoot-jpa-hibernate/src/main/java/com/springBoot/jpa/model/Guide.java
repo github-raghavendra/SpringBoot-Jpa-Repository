8542df2fde1c020fc031f6609f9d91b747df5faf
github-raghavendra <org.raghav@gmail.com>
2021-06-17 15:39:19 +0530

    Student and Guide models are added

---
+++ springBoot-jpa-hibernate/src/main/java/com/springBoot/jpa/model/Guide.java
@@ -0,0 +1,9 @@
+package com.springBoot.jpa.model;
+
+public class Guide {
+
+	private Long id;
+	private String firstName;
+	private String lastName;
+	private Integer age;
+}
---
+++ springBoot-jpa-hibernate/src/main/java/com/springBoot/jpa/model/Student.java
@@ -0,0 +1,10 @@
+package com.springBoot.jpa.model;
+
+public class Student {
+
+	private Long id;
+	private String firstName;
+	private String lastName;
+	private Integer age;
+
+}
