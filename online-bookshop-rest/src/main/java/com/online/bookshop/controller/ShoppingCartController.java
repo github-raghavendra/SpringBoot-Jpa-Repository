2e7a91dbbd778c6f284d81988a8c61d66f4630c9
github-raghavendra <org.raghav@gmail.com>
2021-07-23 01:34:27 +0530

    updated on 23-jul-2021

--- online-bookshop-rest/src/main/java/com/online/bookshop/controller/ShoppingCartController.java
+++ online-bookshop-rest/src/main/java/com/online/bookshop/controller/ShoppingCartController.java
@@ -3,14 +3,19 @@ package com.online.bookshop.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.online.bookshop.model.Book;
+import com.online.bookshop.model.CartItem;
 
 @RestController
 @RequestMapping("/shoppingCart")
 public class ShoppingCartController {
 	
 	@Autowired
+	
 
 	@GetMapping("/addToCart")
 	public void getCart() {
@@ -18,8 +23,11 @@ public class ShoppingCartController {
 	}
 	
 	@PostMapping("/addToCart")
-	public void addToCart() {
-		
+	public void addToCart(@RequestBody Book book, int qty) {
+		CartItem cartItem = new CartItem();
+		cartItem.setBook(book);
+		cartItem.setQty(qty);
+		cartItem.setSubtotal(null);
 	}
 	
 	
